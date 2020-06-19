b40b8a1566f273165b52355eadcc804a3c750e65
PRAJJWAL SINGH <prajjwal875@gmail.com>
2020-06-19 21:17:16 +0530

    Create sample.cpp

---
+++ sample.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+void c_p_c()
+{
+  
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+    #endif
+}
+int main()
+{
+	c_p_c();
+	string s;
+	cin>>s;
+	cout<<s;
+	return 0;
+}
